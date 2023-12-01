5798a7a12cb1266d40e0634880cfcb16ee9098e8
Steven Zhang <1581647952@qq.com>
2023-12-01 17:01:25 +0000

    chore(build): Add markdown vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import Markdown from "unplugin-vue-markdown/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/], // <-- allows Vue to compile Markdown files
+    }),
+    Markdown({
+      /* options */
+    }),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
