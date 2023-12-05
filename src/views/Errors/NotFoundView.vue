d1a3c8f5c2f0626835ab0773497d4f515be8d7c0
Steven Zhang <1581647952@qq.com>
2023-12-05 19:14:53 +0000

    feat(views): add 404 router

---
+++ src/views/Errors/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="flex flex-col h-full w-full mx-auto items-center justify-center">
+        <i class="fa-solid fa-circle-exclamation fa-xl text-6xl"></i>
+        <span class="font-extrabold text-6xl my-10">404</span>
+        <span class="text-4xl mb-10">You reach the end of the world</span>
+        <Buttons type="neutral"><router-link to="/">Return</router-link></Buttons>
+    </h1>
+</template>
+<script lang="ts">
+import Buttons from '../../ui/Buttons.vue';
+
+export default {
+    name: 'NotFound',
+    components:{
+        Buttons
+    }
+}
+</script>
